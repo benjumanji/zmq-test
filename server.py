b4db6982c07f90f12e701c8900bcb7417b85e586
Ben Edwards <edwards.benj@gmail.com>
2013-04-09 00:00:39 +0100

    bind server to localhost, properly

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import time
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://localhost:4001")
+socket.bind("tcp://127.0.0.1:4001")
 
 while True:
     #  Wait for next request from client
